87decd1637712311aac3175f87417bec1ec99513
Meghana <meghanadamerla2308@gmail.com>
2020-02-21 21:16:51 -0800

    Spring-web-mvc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,71 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './views/home/home.component';
+import { ShopComponent } from './views/shop/shop.component';
+import { CollectionComponent } from './views/collection/collection.component';
+import { ContactComponent } from './views/contact/contact.component';
 
-const routes: Routes = [];
+import { LoginComponent } from './views/login/login.component';
+
+import { RegisterComponent } from './views/register/register.component';
+
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+    data: {
+      title: "Home"
+    }
+  },
+  {
+    path: "shop",
+    component: ShopComponent,
+    data: {
+      title: "shop"
+    }
+  },
+  {
+    path: "collection",
+    component: CollectionComponent,
+    data: {
+      title: "collection"
+    }
+  },
+  {
+    path: "contact",
+    component: ContactComponent,
+    data: {
+      title: "Contact"
+    }
+  },
+  
+  {
+    path: "login",
+    component: LoginComponent,
+    data: {
+      title: "Login"
+    }
+  },
+    {
+      path: "register",
+      component: RegisterComponent,
+      data: {
+        title: "Register"
+      }
+    }
+ 
+  
+  
+
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//components
+import { HeaderComponent } from './views/_layout/header/header.component';
+import { FooterComponent } from './views/_layout/footer/footer.component';
+
+
+import { HomeComponent } from './views/home/home.component';
+import { ShopComponent } from './views/shop/shop.component';
+import { CollectionComponent } from './views/collection/collection.component';
+import { ContactComponent } from './views/contact/contact.component';
+
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ShopComponent,
+    CollectionComponent,
+    ContactComponent,
+   
+    LoginComponent,
+   
+    RegisterComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,AppRoutingModule]
 })
 export class AppModule { }
